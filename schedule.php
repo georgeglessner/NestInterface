0530efcb586e7c5c9b755220fc701e493255138c
root <g.glessner@outlook.com>
2019-12-21 11:46:45 -0700

    Update schedule to acocunt for no schedule set or not every day set

--- schedule.php
+++ schedule.php
@@ -4,7 +4,8 @@ require_once('header.php');
 // Get schedule
 $schedule = $nest->getDeviceSchedule();
 
-$daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+// Get days of week in schedule
+$daysOfWeek = array_keys($schedule);
 
 ?>
 <html>
@@ -17,16 +18,22 @@ $daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 	</html>
 <?php
 
-for($i=0; $i<count($daysOfWeek); $i++){
-	for($j=0; $j<count($schedule[$daysOfWeek[$i]]); $j++){
-		echo("<tr>");
-			echo("<td>".$daysOfWeek[$i]."</td>");
-			echo("<td>".convertTime(($schedule[$daysOfWeek[$i]][$j]->time / 60))."</td>");
-			echo("<td>".round($schedule[$daysOfWeek[$i]][$j]->target_temperature)."</td>");
-			echo("<td>".$schedule[$daysOfWeek[$i]][$j]->mode."</td>");
-		echo("</tr>");
-	}
+// Create table
+if($schedule){
+	for($i=0; $i<count($schedule); $i++){
+			for($j=0; $j<count($schedule[$daysOfWeek[$i]]); $j++){
+				echo("<tr>");
+					echo("<td>".$daysOfWeek[$i]."</td>");
+					echo("<td>".convertTime(($schedule[$daysOfWeek[$i]][$j]->time / 60))."</td>");
+					echo("<td>".round($schedule[$daysOfWeek[$i]][$j]->target_temperature)."</td>");
+					echo("<td>".$schedule[$daysOfWeek[$i]][$j]->mode."</td>");
+				echo("</tr>");
+			}
+		}
+}else{
+	echo("<tr><td colspan='4'> No schedule set </td></tr>");
 }
+
 echo("</table>");
 
 
